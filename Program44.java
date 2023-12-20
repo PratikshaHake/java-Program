1c5e51456b68e6693c80befefe505e9f81bb9d54
Pratiksha Pandharinath Hake <122038101+PratikshaHake@users.noreply.github.com>
2023-12-20 13:15:19 +0530

    Update Program44.java

--- Program44.java
+++ Program44.java
@@ -5,12 +5,24 @@ import java.util.*;
 class number{
     void Display(int iNo)
     {
-
+    
+        while(iNo>0)
+        {
+            System.out.print(iNo%10);
+            iNo=iNo/10;
+        }
     }
 }
 public class Program44 {
     public static void main(String arg[])
-    {
+    { 
+        Scanner sobj=new Scanner(System.in);
+        number nobj=new number();
+        int iNo=0;
+        System.out.println("Enter the number :");
+        iNo=sobj.nextInt();
+        nobj.Display(iNo);
+
 
     }
     
